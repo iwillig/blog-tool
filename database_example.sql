c2e34dc8885c18d412a8a42461d7ffa04c10838e
Ivan Willig <iwillig@gmail.com>
2024-07-23 19:37:24 -0400

    Add basic database schema and examples

--- database_example.sql
+++ database_example.sql
@@ -10,6 +10,19 @@ select load_extension('./closure');
 
 BEGIN TRANSACTION;
 
+insert into author (id, name, author_email)
+values (1, 'ivan', 'ivan@example.com')
+returning *;
+
+insert into post (id, title, content, author_id)
+values (1, 'How to build a blog software', '....', 1)
+returning *;
+
+insert into comment (id, content, post_id, author_id, parent_id)
+values
+(1, 'This is a comment', 1, 1, Null),
+(2, 'This is a reply to that the first comment', 1, 1, 1)
+returning *;
 
 
 
--- database_schema.sql
+++ database_schema.sql
@@ -3,18 +3,40 @@ PRAGMA foreign_keys = ON;
 select load_extension('./uuid');
 select load_extension('./closure');
 
-
+create table author (
+       id integer primary key,
+       created_at datetime default (datetime('now')) not null,
+       last_updated_at datetime default (datetime('now')) not null,
+       public_id uuid unique default (uuid()) not null,
+       name text unique,
+       author_email text unique
+);
 
 create table if not exists post (
        id integer primary key asc,
        created_at datetime default (datetime('now')) not null,
-       public_id uuid unique default (uuid()) not null
-
+       last_updated_at datetime default (datetime('now')) not null,
+       public_id uuid unique default (uuid()) not null,
+       title text,
+       content text,
+       author_id integer references author
 );
-
 
 create table if not exists comment (
        id integer primary key asc,
        created_at datetime default (datetime('now')) not null,
-       public_id uuid unique default (uuid()) not null
+       last_updated_at datetime default (datetime('now')) not null,
+       public_id uuid unique default (uuid()) not null,
+       content text,
+       post_id integer references post,
+       parent_id integer references comment,
+       author_id integer references author
+);
+
+create index comment_idx1 on comment(parent_id);
+
+create virtual table comment_hierarchy using transitive_closure(
+       tablename='comment',
+       idcolumn='id',
+       parentcolumn='parent_id'
 );
