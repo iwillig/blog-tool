26a7ce87a5be90290c95c75e30a7a3ee8942847a
Ivan Willig <iwillig@gmail.com>
2024-07-23 19:51:58 -0400

    Fix lint error

--- test/blog_tool/main_test.clj
+++ test/blog_tool/main_test.clj
@@ -2,7 +2,7 @@
   (:require
    [clojure.test :refer [deftest is]]
    [matcher-combinators.test]
-   [matcher-combinators.matchers :as m]))
+   #_[matcher-combinators.matchers :as m]))
 
 (deftest test-okay
   (is (match? false true)))
